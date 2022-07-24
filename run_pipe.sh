a54a0bfc48c0ab987bc923c65c1694c058d90973
ZehanDai <zehan.dai@outlook.com>
2022-07-24 21:36:09 +0800

    commit files for basic functions

---
+++ run_pipe.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e 
+
+ind_reads=$1
+final_oud=$2
+
+
+[ ! -d $ind_reads ] && echo ERROR: please provide input path && exit 
+[ ${#final_oud} -eq 0 ] && final_oud="output/final_oud"
+
+# set up 
+echo "ind_reads: $ind_reads" > config/IO.yaml
+echo "final_oud: $final_oud" >> config/IO.yaml
+
+# pull database files
+database="database"
+bash setup_database.sh $database 
+
+# run pipeline 
+snakemake -p -s snakefile -c all
+
+
+
