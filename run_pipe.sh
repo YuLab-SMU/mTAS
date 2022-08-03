55ba787249c4d730ea789e8ce14dc5a9f3d81e01
ZehanDai <zehan.dai@outlook.com>
2022-08-03 11:30:09 +0800

    automatically select faster source for downloading database file

--- run_pipe.sh
+++ run_pipe.sh
@@ -4,6 +4,7 @@ set -e
 ind_reads=$1
 final_oud=$2
 
+
 [ ${#ind_reads} -eq 0 ] && echo ERROR: please provide input path && exit  
 [ ! -d $ind_reads ] && echo ERROR: please provide input path && exit 
 [ ${#final_oud} -eq 0 ] && final_oud="output/final_oud"
@@ -15,7 +16,7 @@ echo "final_oud: $final_oud" >> config/IO.yaml
 # pull database files
 database="database"
 bash setup_database.sh $database 
-
+ 
 # run pipeline 
 snakemake -p -s snakefile -c all
 
--- setup_database.sh
+++ setup_database.sh
@@ -4,72 +4,75 @@ set -e
 # Descirption:
 #   check, download and setup some of the database files 
 
-# Update
-#   20220721
-
+link1=https://github.com/YuLab-SMU/mTAS.git
+link2=https://gitee.com/wolfgang1989/mTAS.git
 wd=$1 
+
 [ ${#@} -eq 0 ] && wd=database
-mkdir -p $wd 
-
-# Pfam 
-version="current_release"
-root="http://ftp.ebi.ac.uk/pub/databases/Pfam"
-hmm="$root/$version/Pfam-A.hmm.gz"
-md5="$root/$version/md5_checksums"
+mkdir -p `pwd`/$wd 
 p=`pwd`
+pfamd=$p/$wd/Pfam
 
-mkdir -p $wd/Pfam
-
-
-# md5sum file
-[ ! -f $wd/Pfam/md5_checksums ] && wget -c $md5 -O $wd/Pfam/md5_checksums 
-sed -n  "/Pfam-A.hmm.gz/p" $wd/Pfam/md5_checksums > $wd/Pfam/md5.tmp
-
-# hmm.gz
-if [ ! -f $wd/Pfam/Pfam-A.hmm ] ;then 
-    wget -c $hmm -O $wd/Pfam/Pfam-A.hmm.gz 
-    gzip -k -d $wd/Pfam/Pfam-A.hmm.gz # no need to depress, viralVerify accepts gz file 
-    cd $wd/Pfam 
-    md5sum -c md5.tmp | tee $p/log
-    check=`cut -f2 -d' ' $p/log`
-    if [ $check != 'OK' ];then
-    	echo ERROR: Pfam.hmm file is not complete 
-    	exit
-    else
-    	echo downloaded Pfam-A.hmm.gz is complete
-    fi
-    cd $p
+# pull curated database from gh-page branch  
+if [ ! -f $wd/CDTAS/database/md5sum ]; then 
+  bash $p/utils/ping_git_src.sh # ping and compare delays  
+  src=`cat label.tmp` 
+   
+  if [ "$src" == 'gitee' ]; then
+      link=$link2 # select url based on delay
+  else
+      link=$link1 
+  fi 
+  git clone -b gh-pages $link $wd/CDTAS
 fi
 
- 
-
-# pull database from gh-page branch 
-link=https://github.com/YuLab-SMU/mTAS.git 
-git clone -b gh-pages $link $wd/mTAS-db
-mv $wd/mTAS-db/database/* $wd 
-rm -r $wd/mTAS-db 
-
-# unzip database 
-cd $wd 
+#t unzip database 
+cd $wd/CDTAS/database 
 md5sum -c <<< `cat -v md5sum | sed "s/\^M//"` >$p/log 2>&1
-
+ 
 check=`cut -f2 -d' ' $p/log | sort | uniq`
+ 
 if [ $check != 'OK' ];then
     echo ERROR: database file is not complete, you may try re-downloading
     echo "`grep -v 'OK' $p/log `" | sed 's/^/  /g'
-    exit
-else  
+else
     [ ! -d Rfam ] && tar -xf Rfam.tar.gz
     [ ! -d T1TAdb ] && tar -xf T1TAdb.tar.gz
     [ ! -d TADB2 ] && tar -xf TADB2.tar.gz
     [ ! -d TASmania_HMMs ] && tar -xf TASmania_HMMs.tar
     [ ! -d ICEberg/T4SS ]  && cd ICEberg && tar -xf T4SS.tar.gz && tar -xf ICE.tar.gz && tar -xf AICE.tar.gz && tar -xf IME.tar.gz && tar -xf CIME.tar.gz
 fi
+ 
 
 
-DB_ICEberg=$p/$wd/ICEberg
-DB_Rfam=$p/$wd/Rfam
-DB_T1TAdb=$p/$wd/T1TAdb
-DB_TADB2=$p/$wd/TADB2
-DB_TASmania=$p/$wd/TASmania
+# download Pfam
+version="current_release"
+root="http://ftp.ebi.ac.uk/pub/databases/Pfam"
+hmm="$root/$version/Pfam-A.hmm.gz"
+md5="$root/$version/md5_checksums"
 
+mkdir -p $pfamd
+
+# md5sum file 
+#ouf_md5=$p/$wd/Pfam/md5_checksums 
+ouf_md5=$pfamd/md5_checksums 
+[ ! -f $ouf_md5 ] && wget -c $md5 -O $ouf_md5
+ 
+sed -n "/Pfam-A.hmm.gz/p" $ouf_md5 > $pfamd/md5.tmp
+  
+if [ ! -f $pfamd/Pfam-A.hmm ] ; then
+    wget -c $hmm -O $pfamd/Pfam-A.hmm.gz
+    gzip -k -d $pfamd/Pfam-A.hmm.gz # no need to depress, viralVerify accepts gz file
+    cd $pfamd
+    md5sum -c md5.tmp | tee $p/log
+    check=`cut -f2 -d' ' $p/log`
+    if [ $check != 'OK' ];then
+	echo ERROR: Pfam.hmm file is not complete
+	exit
+    else
+	echo downloaded Pfam-A.hmm.gz is complete
+    fi
+    cd $p
+fi
+
+rm -f $p/label.tmp $p/log 
